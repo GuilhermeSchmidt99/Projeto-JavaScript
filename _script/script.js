bd82321014ea41b2872b58a2b18876d534bb530c
Schmidt <102011840+GuilhermeSchmidt99@users.noreply.github.com>
2022-07-23 01:48:11 -0300

    update do index e do js

---
+++ _script/script.js
@@ -0,0 +1,6 @@
+//Variáveis começam com uma letra minúscula;
+//Objetos e classes começam com a letra maiúscula.
+//Constantes são escritas em CAPS LOCK (ainda não temos um exempo aqui).
+var dia = new Date();
+document.body.innerHTML = "<h1> Hoje é: " + dia + "</h1>";
+// Mostra o dia, mês, ano, horário, local e mais algumas informaçoes
